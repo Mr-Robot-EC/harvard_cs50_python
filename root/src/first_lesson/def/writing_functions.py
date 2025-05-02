671aa49ac6d74d138fc9332701a635db6c8ead6b
Andrea Edson Lorenzoni <andreaedson93@gmail.com>
2025-05-02 21:23:25 +0100

    Separation of concerns. All the logic has been written inside functions.

--- root/src/first_lesson/def/writing_functions.py
+++ root/src/first_lesson/def/writing_functions.py
@@ -1,57 +1,73 @@
-from math import floor
+import sys
 
+def main():
 
+    print_tree()
+
+    separate_line(15)
+    name = input("What is your name? ")
+    hello(name)
+
+    separate_line(15)
+    hello()
+
+    separate_line(15)
+
+    is_main_running = input("Do you want to stop the main function? (y/n): ")
+
+    if is_main_running == "y":
+        print("Exiting main program..")
+        sys.exit()
 def hello(to="world"):
     print(f"Hello, {to}!")
-
+def separate_line(n):
+    print("\n" + "*" * int(n) + "\n")
 def print_tree(floors="3", trunk="5"):
 
-    floors = int(floors)
-    trunk = int(trunk)
+    is_print_tree_on = True
+    # print a tree
+    while is_print_tree_on:
 
-    space = " " * floors
-    base = space + "*"
-    asterisk = "*"
-    trunk_space = (floors - 1) * " "
+        # checking values (defensive programming)
+        try:
+            tree_array = input(
+                "How many floors and what is the trunk height of your tree? \nplease write your numbers separated by a comma: ").replace(
+                " ", "").split(",")
+            # len_check = True if len(tree_array) == 2 else False
+            # type_check = True if (type(int(tree_array[0])) == int and type(int(tree_array[1])) == int) else False
 
-    for i in range(floors):
-        #floors -= 1
-        space = " " * (floors - i - 1)
-        asterisk += "*" * 2
-        base += ("\n" + space + asterisk)
+            len_check = len(tree_array) == 2
+            type_check = all(isinstance(int(num), int) for num in tree_array)
 
-    print(base)
+            if len_check and type_check:
 
-    for i in range(trunk):
-        print(trunk_space + "**")
+                floors = int(tree_array[0])
+                trunk = int(tree_array[1])
 
+                space = " " * floors
+                base = space + "*"
+                asterisk = "*"
+                trunk_space = (floors - 1) * " "
 
-#playing with def functions
-name = input("What is your name? ")
-hello(name)
-hello()
+                for i in range(floors):
+                    # floors -= 1
+                    space = " " * (floors - i - 1)
+                    asterisk += "*" * 2
+                    base += ("\n" + space + asterisk)
 
-print("\n" + "*" * 10 + "\n")
+                print(base)
 
-#print a tree
-while True:
-    try:
-        tree_array = input(
-            "How many floors and what is the trunk height of your tree? \nplease write your numbers separated by a comma: ").replace(
-            " ", "").split(",")
-        # len_check = True if len(tree_array) == 2 else False
-        # type_check = True if (type(int(tree_array[0])) == int and type(int(tree_array[1])) == int) else False
+                for i in range(trunk):
+                    print(trunk_space + "**")
 
-        len_check = len(tree_array) == 2
-        type_check = all(isinstance(int(num), int) for num in tree_array)
+                is_print_tree_on = False
+            else:
+                print("Please enter two numbers separated by a comma")
 
-        if len_check and type_check:
-            print_tree(tree_array[0], tree_array[1])
-            break
-        else:
-            print("Please enter two numbers separated by a comma")
-
-    except Exception as e:
-        print("An exception occurred:", e)
+        except Exception as e:
+            print("An exception occurred:", e)
 
 
+if __name__ == '__main__':
+    while True:
+        main()
